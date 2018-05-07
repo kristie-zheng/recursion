442c742bdda03bf790c34a94f71d4b8a38ad897f
Kristie Desiree <kly5@mail.sfsu.edu>
2018-05-06 22:20:07 -0700

    Removed if where n===0 because it never reaches zero

--- 01factorial.js
+++ 01factorial.js
@@ -1,11 +1,10 @@
 //input: an integer
 //output: integer representing the multiplication of n * n-1 * ... 1
-//constraints: must be positive
+//constraints: must be positive int
 //edge cases: no
 
-var total = 1;
 var factorial = function (n) {
-  if (n === 1 || n === 0) {
+  if (n === 1) {
     return 1;
   }
   else {
@@ -13,4 +12,4 @@ var factorial = function (n) {
   }
 }
 
-console.log(factorial(3))
+console.log(factorial(150))
